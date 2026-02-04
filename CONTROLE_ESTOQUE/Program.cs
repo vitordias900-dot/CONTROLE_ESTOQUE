038c60aa5db81151ac69fed9d5ae6ddb8bfbdeee
vitordias900-dot <vitordias900@gmail.com>
2026-02-04 09:43:31 -0300

    modificado

--- CONTROLE_ESTOQUE/Program.cs
+++ CONTROLE_ESTOQUE/Program.cs
@@ -3,43 +3,76 @@ using System.Globalization;
 
 namespace Programa
 {
-    class Program
+    class Produto
     {
+        public string Nome;
+        public int Comprado;
+        public int Vendido;
+
+
         static void Main(string[] args)
         {
             CultureInfo CI = CultureInfo.InvariantCulture;
 
             Console.WriteLine("===== CONTROLE DO ESPETO =====");
 
-            
-            Console.Write("Nome do produto: ");
-            string produto = Console.ReadLine();
+            List<Produto> lista = new List<Produto>();
 
-            
-            Console.Write("Quantidade comprada: ");
-            int quantidadeComprada = int.Parse(Console.ReadLine());
+            bool continuar = true;
 
-           
-            Console.Write("Quantidade vendida: ");
-            int quantidadeVendida = int.Parse(Console.ReadLine());
-
-            if (quantidadeVendida > quantidadeComprada)
+            while (continuar)
             {
-                Console.WriteLine("ERRO: Você vendeu mais do que comprou!");
-                return;
+                Console.Write("\nPRODUTO: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("QUANTIDADE COMPRADA: ");
+                int comprado = int.Parse(Console.ReadLine());
+
+                Console.Write("QUANTIDADE VENDIDA: ");
+                int vendido = int.Parse(Console.ReadLine());
+
+                Produto p = new Produto();
+                p.Nome = nome;
+                p.Comprado = comprado;
+                p.Vendido = vendido;
+
+                lista.Add(p);
+
+                Console.Write("\nDeseja cadastrar outro produto? (S/N): ");
+                string resposta = Console.ReadLine();
+
+                if (resposta.ToUpper() == "N")
+                {
+                    continuar = false;
+                }
             }
 
-            
-            int estoqueFinal = quantidadeComprada - quantidadeVendida;
-            double faturamento = quantidadeVendida * 8.0;
 
             Console.WriteLine("\n===== RELATÓRIO DO DIA =====");
-            Console.Write("\nProduto: " + produto);
-            Console.Write("\nComprado: " + quantidadeComprada);
-            Console.Write("\nVendido: " + quantidadeVendida);
-            Console.Write("\nSobra: " + estoqueFinal);
-            Console.Write("\nFaturamento: R$ " + faturamento.ToString("F2", CI));
-            Console.WriteLine();
+
+            double faturamentoTotal = 0;
+            int sobraTotal = 0;
+
+            foreach (Produto p in lista)
+            {
+                int sobra = p.Comprado - p.Vendido;
+                double faturamento = p.Vendido * 8.0;
+           
+
+                sobraTotal = sobraTotal + sobra;
+                faturamentoTotal = faturamentoTotal + faturamento;
+
+                Console.WriteLine("\nProduto: " + p.Nome);
+                Console.WriteLine("Comprado: " + p.Comprado);
+                Console.WriteLine("Vendido: " + p.Vendido);
+                Console.WriteLine("Sobra: " + sobra);
+                Console.WriteLine("Faturamento: R$ " + faturamento.ToString("F2", CI));
+                Console.WriteLine("------------------------");
+                
+            }
+
+            Console.WriteLine("\nTOTAL DO DIA = R$ " + faturamentoTotal.ToString("F2", CI));
+            Console.WriteLine("SOBRA TOTAL = " + sobraTotal);
         }
     }
 }
